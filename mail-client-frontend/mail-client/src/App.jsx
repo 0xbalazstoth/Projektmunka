efabd6381971f1de38284007b90800c67eafb7c4
Balázs Tóth <grandmastar27@gmail.com>
2023-09-19 20:40:12 +0200

    Added base web frontend

--- mail-client-frontend/mail-client/src/App.jsx
+++ mail-client-frontend/mail-client/src/App.jsx
@@ -1,35 +1,47 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import "./App.css";
+import Sidebar, { SidebarItem } from "./components/SidebarWithContentSeparator";
+import { LayoutDashboard } from "lucide-react";
+import Main from "./components/Main";
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	return (
+		// <main className="App layout">
+		// 	{/* <Sidebar>
+		// 		<SidebarItem
+		// 			icon={<LayoutDashboard size={20}></LayoutDashboard>}
+		// 			text={"Dashboard"}
+		// 			alert
+		// 		></SidebarItem>
+		// 		<SidebarItem
+		// 			icon={<LayoutDashboard size={20}></LayoutDashboard>}
+		// 			text={"Inbox"}
+		// 		></SidebarItem>
+		// 		<SidebarItem
+		// 			icon={<LayoutDashboard size={20}></LayoutDashboard>}
+		// 			text={"Spam"}
+		// 		></SidebarItem>
+		// 		<hr className="my-3"></hr>
+		// 		<SidebarItem
+		// 			icon={<LayoutDashboard size={20}></LayoutDashboard>}
+		// 			text={"Settings"}
+		// 		></SidebarItem>
+		// 		<SidebarItem
+		// 			icon={<LayoutDashboard size={20}></LayoutDashboard>}
+		// 			text={"Help"}
+		// 		></SidebarItem>
+		// 	</Sidebar> */}
+		// 	<Main>
+		// 		<div>content</div>
+		// 		<footer>footer</footer>
+		// 	</Main>
+		// </main>
+		<Main>
+			<div>content</div>
+			<footer>footer</footer>
+		</Main>
+	);
 }
 
-export default App
+export default App;
---
+++ mail-client-frontend/mail-client/src/components/SidebarWithContentSeparator.jsx
@@ -0,0 +1,103 @@
+﻿/* eslint-disable react/prop-types */
+import { ChevronLast, ChevronFirst } from "lucide-react";
+import { useContext, createContext, useState, useEffect } from "react";
+
+const SidebarContext = createContext();
+
+export default function Sidebar({ children }) {
+	const [expanded, setExpanded] = useState(true);
+	const [activeItem, setActiveItem] = useState(null);
+
+	const handleItemClick = (item) => {
+		setActiveItem(item);
+	};
+
+	// Add a useEffect to update the 'expanded' state based on window width
+	useEffect(() => {
+		const handleResize = () => {
+			if (window.innerWidth < 475) {
+				setExpanded(false);
+			} else {
+				setExpanded(true);
+			}
+		};
+
+		handleResize(); // Initial check
+		window.addEventListener("resize", handleResize);
+
+		return () => {
+			window.removeEventListener("resize", handleResize);
+		};
+	}, []);
+
+	return (
+		<nav className="min-h-full flex-col bg-white border-b border-r shadow-sm">
+			<div className="p-4 pb-2 flex justify-between items-center">
+				<button
+					onClick={() => setExpanded((curr) => !curr)}
+					className="p-1.5 rounded-lg bg-gray-50 hover:bg-gray-100"
+				>
+					{expanded ? <ChevronFirst /> : <ChevronLast />}
+				</button>
+			</div>
+
+			<SidebarContext.Provider
+				value={{ expanded, activeItem, handleItemClick }}
+			>
+				<ul className="flex-1 px-3">{children}</ul>
+			</SidebarContext.Provider>
+		</nav>
+	);
+}
+
+export function SidebarItem({ icon, text, alert }) {
+	const { expanded, activeItem, handleItemClick } =
+		useContext(SidebarContext);
+
+	const isActive = activeItem === text;
+
+	return (
+		<li
+			className={`
+        relative flex items-center py-2 px-3 my-1
+        font-medium rounded-md cursor-pointer
+        transition-colors group
+        ${
+			isActive
+				? "bg-gradient-to-tr from-indigo-200 to-indigo-100 text-indigo-800"
+				: "hover:bg-indigo-50 text-gray-600"
+		}
+    `}
+			onClick={() => handleItemClick(text)}
+		>
+			{icon}
+			<span
+				className={`overflow-hidden transition-all ${
+					expanded ? "w-52 ml-3" : "w-0"
+				}`}
+			>
+				{text}
+			</span>
+			{alert && (
+				<div
+					className={`absolute right-2 w-2 h-2 rounded bg-indigo-400 ${
+						expanded ? "" : "top-2"
+					}`}
+				/>
+			)}
+
+			{!expanded && (
+				<div
+					className={`
+          absolute left-full rounded-md px-2 py-1 ml-6
+          bg-indigo-100 text-indigo-800 text-sm
+          opacity-0 translate-x-3 transition-all
+          group-hover:opacity-100 group-hover:translate-x-0
+      `}
+				>
+					{text}
+				</div>
+			)}
+		</li>
+	);
+}
